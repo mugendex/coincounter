9ff5b8d0867cdde9c23772269741e90f1f08597e
mugendex <tanariku6134@gmail.com>
2018-06-08 13:47:02 +0900

    nodeモジュールが中にあったので外へ。

--- script/app.js
+++ script/app.js
@@ -30,102 +30,44 @@ function decrement(out){
 
 
 
-// コインカウンターを追加する
+// コインカウンターを追加するメソッド
 function createCC(parentId){
     //まだ存在しない場合
     if(document.getElementById(`${parentId}_CC`) == null){
         var parent = document.getElementById(parentId);
 
-                // 本体
-                var Pn = document.createElement("div");
-                Pn.classList.add(`CCWrapper`);
-                Pn.id = `${parentId}_CC`
-                parent.appendChild(Pn);
+            // 本体
+            var Pn = document.createElement("div");
+            Pn.classList.add(`CCWrapper`);
+            Pn.id = `${parentId}_CC`;
+            parent.appendChild(Pn);
         
-                // 値表示部
-                var counter = document.createElement("div");
-                counter.classList.add(`${parentId}_CC`);
-                counter.id = `${parentId}CC_out`;
-                counter.textContent = "0";
+            // 値表示部
+            var counter = document.createElement("div");
+            counter.classList.add(`${parentId}_CC`);
+            counter.id = `${parentId}CC_out`;
+            counter.textContent = "0";
                 
         
-                // +ボタン
-                var plusButton = document.createElement("button");
-                plusButton.textContent = "＋";
-                plusButton.onclick = function(){
-                        increment(`${parentId}CC_out`);
-                };
+            // +ボタン
+            var plusButton = document.createElement("button");
+            plusButton.textContent = "＋";
+            plusButton.onclick = function(){
+                increment(`${parentId}CC_out`);
+            };
     
-                // -ボタン
-                var minusButton = document.createElement("button");
-                minusButton.textContent = "－";
-                minusButton.onclick = function(){
-                        decrement(`${parentId}CC_out`);
-                }
+            // -ボタン
+            var minusButton = document.createElement("button");
+            minusButton.textContent = "－";
+            minusButton.onclick = function(){
+                decrement(`${parentId}CC_out`);
+            }
                 
                 //下につける
-                Pn.appendChild(counter);
-                Pn.appendChild(plusButton);
-                Pn.appendChild(minusButton);
-            }
-    
-
-    /*if(parentId == "P1" && document.getElementById("P1_CC") == null){
-
-        // 本体
-        var P1CC = document.createElement("div");
-        P1CC.setAttribute("class","CCWrapper P1_CC");
-        parent.appendChild(P1CC);
-
-        // 値表示部
-        var counter = document.createElement("div");
-        counter.classList.add = "P1_CC";
-        counter.id = "P1CC_out";
-        counter.textContent = "0";
-
-        var P1CCFunc = new CoinCounter("P1CC_out");
-
-        // +ボタン
-        var plusButton = document.createElement("button");
-        plusButton.textContent = "+";
-        plusButton.onclick = P1CCFunc.increment;
-
-        // -ボタン
-        var minusButton = document.createElement("button");
-        minusButton.textContent = "-";
-        minusButton.setAttribute("onclick","P1CCFunc.decrement()");
-
-        P1CC.appendChild(counter);
-        P1CC.appendChild(plusButton);
-        P1CC.appendChild(minusButton);
-
-        
+            Pn.appendChild(counter);
+            Pn.appendChild(plusButton);
+            Pn.appendChild(minusButton);
     }
-
-    if(parentId == "P2" && document.getElementById("P2_CC") == null){
-         var P2CCfunc = new CoinCounter("P2CC_out");
-
-         // 本体
-         var P2CC = document.createElement("div");
-         P2CC.setAttribute("class","CCWrapper P2_CC");
-         parent.appendChild(P2CC);
-
-         // 値表示部
-         var counter = document.createElement("div");
-         counter.setAttribute("class","Coin_Counter");
-         counter.setAttribute("id","P2CC_out");
-         counter.textContent = "0";
- 
-         // +ボタン
-         var plusButton = document.createElement("button");
-         plusButton.setAttribute("onclick","P2CCfunc.increment()");
- 
-         // -ボタン
-         var minusButton = document.createElement("button");
-         minusButton.setAttribute("onclick","P2CCfunc.decrement()");
-
-         P2CC.appendChild(counter,plusButton,minusButton);
-    }*/
 }
 
 /**************************************************
